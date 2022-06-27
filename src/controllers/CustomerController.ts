c69ae2412030de03fce6963a3e2b450288db2a70
--add <tarun@iotric.com>
2022-06-27 13:18:53 +0530

    orders

--- src/controllers/CustomerController.ts
+++ src/controllers/CustomerController.ts
@@ -2,8 +2,10 @@ import express, { Request, Response, NextFunction } from "express";
 import { validate } from "class-validator";
 import { plainToClass } from "class-transformer";
 import {
+  CartItem,
   CreateCustomerInput,
   EditCustomerProfileInput,
+  OrderInputs,
   UserLoginInput,
 } from "../dto/Customer.dtos";
 import {
@@ -13,7 +15,14 @@ import {
   ValidatePassword,
 } from "../utility/PasswordUtility";
 import { GenerateOtp } from "../utility/NotificationUtility";
-import { Customer, DeliveryUser, Vendor } from "../models";
+import {
+  Customer,
+  DeliveryUser,
+  Order,
+  Transaction,
+  Vendor,
+  Food,
+} from "../models";
 
 export const CustomerSignUp = async (
   req: Request,
@@ -236,8 +245,6 @@ export const EditCustomerProfile = async (
   return res.status(400).json({ msg: "Error while Updating Profile" });
 };
 
-
-
 // /* ------------------- Delivery Notification --------------------- */
 
 // const assignOrderForDelivery = async(orderId: string, vendorId: string) => {
@@ -257,7 +264,7 @@ export const EditCustomerProfile = async (
 //             const currentOrder = await Order.findById(orderId);
 //             if(currentOrder){
 //                 //update Delivery ID
-//                 currentOrder.deliveryId = deliveryPerson[0]._id; 
+//                 currentOrder.deliveryId = deliveryPerson[0]._id;
 //                 await currentOrder.save();
 
 //                 //Notify to vendor for received new order firebase push notification
@@ -265,21 +272,16 @@ export const EditCustomerProfile = async (
 
 //         }
 
-
 //     }
-
-
-
 
 //     // Update Delivery ID
 
 // }
 
-
 // /* ------------------- Order Section --------------------- */
 
 // const validateTransaction = async(txnId: string) => {
-    
+
 //     const currentTransaction = await Transaction.findById(txnId);
 
 //     if(currentTransaction){
@@ -289,3 +291,127 @@ export const EditCustomerProfile = async (
 //     }
 //     return {status: false, currentTransaction};
 // }
+
+/* ------------------- Order Section --------------------- */
+
+const validateTransaction = async (txnId: string) => {
+  const currentTransaction = await Transaction.findById(txnId);
+  if (currentTransaction) {
+    if (currentTransaction.status.toLowerCase() !== "failed") {
+      return { status: true, currentTransaction };
+    }
+  }
+  return { status: false, currentTransaction };
+};
+
+export const CreateOrder = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const customer = req.user;
+
+  const { txnId, amount, items } = <OrderInputs>req.body;
+
+  if (customer) {
+    const { status, currentTransaction } = await validateTransaction(txnId);
+
+    if (!status) {
+      return res.status(404).json({ message: "Error while Creating Order!" });
+    }
+
+    const profile = await Customer.findById(customer._id);
+
+    const orderId = `${Math.floor(Math.random() * 89999) + 1000}`;
+
+    const cart = <[CartItem]>req.body.items;
+
+    let cartItems = Array();
+
+    let netAmount = 0.0;
+
+    let vendorId;
+
+    const foods = await Food.find()
+      .where("_id")
+      .in(cart.map((item) => item._id))
+      .exec();
+
+    foods.map((food) => {
+      cart.map(({ _id, unit }) => {
+        if (food._id == _id) {
+          vendorId = food.vendorId;
+          netAmount += food.price * unit;
+          cartItems.push({ food, unit });
+        }
+      });
+    });
+
+    if (cartItems) {
+      const currentOrder = await Order.create({
+        orderId: orderId,
+        vendorId: vendorId,
+        items: cartItems,
+        totalAmount: netAmount,
+        paidAmount: amount,
+        orderDate: new Date(),
+        orderStatus: "Waiting",
+        remarks: "",
+        deliveryId: "",
+        readyTime: 45,
+      });
+
+      if (currentOrder) {
+        profile.orders.push(currentOrder._id);
+      }
+
+      currentTransaction.vendorId = vendorId;
+      currentTransaction.orderId = orderId;
+      currentTransaction.status = "CONFIRMED";
+
+      await currentTransaction.save();
+
+      // await assignOrderForDelivery(currentOrder._id, vendorId);
+
+      const profileResponse = await profile.save();
+
+      return res.status(200).json(profileResponse);
+    }
+  }
+
+  return res.status(400).json({ msg: "Error while Creating Order" });
+};
+
+export const GetOrders = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const customer = req.user;
+
+  if (customer) {
+    const profile = await Customer.findById(customer._id).populate("orders");
+    if (profile) {
+      return res.status(200).json(profile.orders);
+    }
+  }
+  return res.status(400).json({ msg: "Orders not found" });
+};
+
+export const GetOrderById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const orderId = req.params.id;
+
+  if (orderId) {
+    const order = await Customer.findById(orderId).populate("items.food");
+
+    if (order) {
+      return res.status(200).json(order);
+    }
+  }
+
+  return res.status(400).json({ msg: "Order not found" });
+};
--- src/models/index.ts
+++ src/models/index.ts
@@ -3,3 +3,6 @@ export * from './Food.model'
 export * from './Offer.model';
 export * from './Customer.model';
 export * from  './DeliveryUser.model';
+export * from './Transaction.model';
+export * from './Order.model';
+export * from './Food.model';
